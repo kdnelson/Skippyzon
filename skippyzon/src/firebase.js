2a39c9d65fb206ac85cd4ae05a9c3e8816968c63
kdnelson <kris.d.nelson@gmail.com>
2023-01-24 15:42:36 -0800

    Added firebase packages and config

---
+++ skippyzon/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5UCQMRHESDEh0h5SO4p2QQBlDPkkvlo0",
+  authDomain: "skippyzon-2ff49.firebaseapp.com",
+  projectId: "skippyzon-2ff49",
+  storageBucket: "skippyzon-2ff49.appspot.com",
+  messagingSenderId: "889009414021",
+  appId: "1:889009414021:web:00fa0e7cac7734c06f6eec",
+  measurementId: "G-2QPLZE9EEH"
+};
