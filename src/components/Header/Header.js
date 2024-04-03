504ad59865df5ba504a62448718abade9e07ccfa
kdnelson <kris.d.nelson@gmail.com>
2024-04-02 21:00:52 -0700

    Header is even better

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,36 +18,38 @@ const Header = () => {
       <div className="row align-items-center">
         <div className="col-md-7">
           <div className="d-flex justify-content-start align-items-center">
-            <div className="col-auto">
+            <div className="col-auto mb-1">
               <Link className="logo-style" to="/">Skippyzon</Link>
             </div>
             <div className="col">
               <div className="d-flex justify-content-end align-items-center ">
-                <input className="col m-2 searchbar-styling" type="text" onChange={(e) => setProductQuery(e.target.value)}/>
+                <input className="col m-2 searchbar-style" type="text" onChange={(e) => setProductQuery(e.target.value)}/>
                 <div className="col-auto"><SearchIcon /></div>
               </div>
             </div>
           </div>
         </div>
-        <div className="col-md-5 mt-1 mb-1 header-link-styling">
-          <div className="d-flex mx-2 justify-content-start align-items-center">
+        <div className="col-md-5 mt-1 mb-1">
+          <div className="d-flex mx-3 justify-content-start align-items-center">
             <div className="col-7">
               <Link className="link-style" to='/login'>
                 <div className="row">{t('header.hello')} {!user ? t('header.guest') : user?.email}</div>
-                <div className="row">{user ? t('header.signOut') : t('header.signIn')}</div>
+                <div className="row fs-6 fw-bold">{user ? t('header.signOut') : t('header.signIn')}</div>
               </Link>
             </div>
             <div className="col-3 mx-2 justify-content-end">
               {user && (
                 <Link className="link-style" to='/orders'>
                   <div className="row">{t('header.returns')}</div>
-                  <div className="row">{t('header.andOrders')}</div>
+                  <div className="row fs-6 fw-bold">{t('header.andOrders')}</div>
                 </Link>)}
             </div>
             <div className="col-2">
-              <Link className="link-style" to="/checkout">
-                <ShoppingBasketIcon />
-                {getCartCounter()}
+              <Link className="link-style shopping-cart-style" to="/checkout">
+                <div className="row">
+                  <div className="col-2"><ShoppingBasketIcon /></div>
+                  <div className="col-auto">{getCartCounter()}</div>  
+                </div>
               </Link>
             </div>
           </div>
