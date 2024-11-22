b94c9d8eea0897fda71ce96c22f9167cee9e2058
kdnelson <kris.d.nelson@gmail.com>
2024-11-21 20:44:29 -0800

    Adding order actions

---
+++ src/middleware/actions/orderActions.jsx
@@ -0,0 +1,41 @@
+import {
+  addOrderStart,
+  addOrderSuccess,
+  addOrderError,
+  removeOrderStart,
+  removeOrderSuccess,
+  removeOrderError,
+  emptyOrderStart,
+  emptyOrderSuccess,
+  emptyOrderError
+} from '../reducers/cartReducer';
+
+export const addOrderAction = (data) => dispatch => {
+  try {
+    dispatch(addOrderStart())
+
+    dispatch(addOrderSuccess(data))
+  } catch (err) {
+    dispatch(addOrderError(err))
+  }
+}
+
+export const removeOrderAction = (data) => dispatch => {
+  try {
+    dispatch(removeOrderStart())
+
+    dispatch(removeOrderSuccess(data))
+  } catch (err) {
+    dispatch(removeOrderError(err))
+  }
+}
+
+export const emptyOrderAction = (data) => dispatch => {
+  try {
+    dispatch(emptyOrderStart())
+
+    dispatch(emptyOrderSuccess(data))
+  } catch (err) {
+    dispatch(emptyOrderError(err))
+  }
+}
