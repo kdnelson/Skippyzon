38885ebcb74c05caff41f9bde47cbc589cd5a433
kdnelson <kris.d.nelson@gmail.com>
2025-03-01 16:03:20 -0800

    Header set well for 790 and less, need to build new format for header less than 790

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 ReactJS with Vite
 
+> npm run dev
+
 TODO
 
 Why are actions getting multiple calls?
 Capitalize folders and component names?
-Reduce padding on cards and align verticallu with header edge
+Reduce padding on cards and align vertically with header edge
 Should key on user id instead of email?
 Create user object in useUser and export
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -14,46 +14,34 @@ const Header = () => {
   const { setProductQuery } = useProduct()
 
   return (
-    <div className="container header-style">
-      <div className="row align-items-center">
-        <div className="col-md-7">
-          <div className="d-flex justify-content-start align-items-center">
-            <div className="col-auto mb-1">
-              <Link className="logo-style" to="/">Skippyzon</Link>
-            </div>
-            <div className="col">
-              <div className="d-flex justify-content-end align-items-center ">
-                <input className="col m-2 searchbar-style" type="text" onChange={(e) => setProductQuery(e.target.value)}/>
-                <div className="col-auto"><SearchIcon /></div>
-              </div>
-            </div>
-          </div>
+    <div className="header-style">
+      <div class="d-flex flex-wrap align-items-start">
+        <div class="p-2 mt-1"><Link className="logo-style" to="/">Skippyzon</Link></div>
+        <div class="d-flex align-items-center mt-1">
+          <input className="mt-2 searchbar-style" type="text" onChange={(e) => setProductQuery(e.target.value)}/>
+          <div className="col-auto ps-1 pe-2 mt-2"><SearchIcon /></div>
         </div>
-        <div className="col-md-5 mt-1 mb-1">
-          <div className="d-flex mx-3 justify-content-start align-items-center">
-            <div className="col-7">
-              <Link className="link-style" to='/login'>
-                <div className="row">{t('header.hello')} {!email ? t('header.guest') : email}</div>
-                <div className="row fs-6 fw-bold">{email ? t('header.signOut') : t('header.signIn')}</div>
-              </Link>
-            </div>
-            <div className="col-3 mx-2 justify-content-end">
-              {email && (
-                <Link className="link-style" to='/orders'>
-                  <div className="row">{t('header.returns')}</div>
-                  <div className="row fs-6 fw-bold">{t('header.andOrders')}</div>
-                </Link>)}
-            </div>
-            <div className="col-2">
-              {getCartCounter() > 0 && (
-                <Link className="link-style shopping-cart-style" to="/checkout">
-                  <div className="row">
-                    <div className="col-2"><ShoppingBasketIcon /></div>
-                    <div className="col-auto">{getCartCounter()}</div>  
-                  </div>
-                </Link> )}
-            </div>
-          </div>
+        <div class="signin-orders-style link-style p-2 pe-5">
+          <Link className="link-style" to='/login'>
+            <div className="row">{t('header.hello')} {!email ? t('header.guest') : email}</div>
+            <div className="row fs-6 fw-bold">{email ? t('header.signOut') : t('header.signIn')}</div>
+          </Link>
+        </div>
+        <div class="signin-orders-style p-2 pe-2">            
+          {email && (
+            <Link className="link-style" to='/orders'>
+              <div className="row">{t('header.returns')}</div>
+              <div className="row fs-6 fw-bold">{t('header.andOrders')}</div>
+            </Link>)}
+        </div>
+        <div class="mt-3 ms-auto">
+          {getCartCounter() > 0 && (
+            <Link className="link-style shopping-cart-style" to="/checkout">
+              <div className="row">
+                <div className="col-4"><ShoppingBasketIcon /></div>
+                <div className="col-auto">{getCartCounter()}</div>  
+              </div>
+            </Link>)}
         </div>
       </div>
     </div>
