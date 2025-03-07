c8632142c698223ee8422b7b288aa245e49e2364
kdnelson <kris.d.nelson@gmail.com>
2025-03-06 21:44:35 -0800

    Nice response header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -69,7 +69,41 @@ const Header = () => {
       )}
       {screenSize < MAX_DESKTOP_HEADER_SCREENSIZE && 
        screenSize > MAX_PHONE_HEADER_SCREENSIZE && (
-        <div>Tablet</div>
+        <div class="row">
+          <div class="d-flex flex-wrap align-items-start">
+            <div className="p-1 pe-2">
+              <div><Link className="logo-style" to="/">Skippyzon</Link></div>
+            <div className="header-version">{reactJsVersion}</div>
+            </div>
+            <div class="d-flex mt-1">
+              <input className="mt-2 tablet-searchbar" type="text" onChange={(e) => setProductQuery(e.target.value)}/>
+              <div className="col-auto ps-1 pe-2 mt-2"><SearchIcon /></div>
+            </div>
+            <div class="mt-3 ms-auto">
+              <Link className="link-style shopping-cart-style" to="/checkout">
+                <div className="row">
+                  <div className="col-3 p-0"><ShoppingBasketIcon /></div>
+                  <div className="col-auto">{getCartCount()}</div>  
+                </div>
+              </Link>
+            </div>
+          </div>
+          <div class="d-flex flex-wrap align-items-start">
+            <div class="signin-orders-style link-style p-2 pe-4">
+              <Link className="link-style" to='/login'>
+                <div className="row">{!email ? t('header.guest') : email}</div>
+                <div className="row fs-6 fw-bold">{email ? t('header.signOut') : t('header.signIn')}</div>
+              </Link>
+            </div>
+            <div class="signin-orders-style p-2 pe-3">            
+              {email && getOrderCount() > 0 && (
+                <Link className="link-style" to='/orders'>
+                  <div className="row">{t('header.returns')}</div>
+                  <div className="row fs-6 fw-bold">{t('header.andOrders')}</div>
+                </Link>)}
+            </div>
+          </div>
+        </div>
       )}
       {screenSize <= MAX_PHONE_HEADER_SCREENSIZE && (
         <div class="row">
